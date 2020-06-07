1cbae950bf71935ac32be3cf15f2d3f1a84c2ab3
SaluKhadka <salukhadka@lftechnology.com>
2020-06-07 17:57:14 +0545

    Add method to add/delete todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Todos from './components/Todo';
+import AddTodo from './components/AddTodo';
+import About from './components/layout/pages/About';
+import Header from './components/layout/Header';
+import uuid from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    todos : [
+      {
+      id: uuid.v4(),
+      title : 'Learn React',
+      completed: false
+    },
+    {
+      id: uuid.v4(),
+      title : 'Cool food',
+      completed: true
+    },
+    {
+      id: uuid.v4(),
+      title : 'Learn React',
+      completed: false
+    }
+ ]
+  }
+
+  toggleComplete = (id) => {
+    this.setState({todo: this.state.todos
+      .filter(todo => todo.id === id)
+      .map(todo => todo.completed = !todo.completed)
+    });
+  }
+
+  deleteTodo = (id) => {
+    console.log(id);
+    this.setState({todos: [...this.state.todos.filter(it => it.id !== id)]});
+  }
+
+  addTodo = (title) => {
+      console.log(title);
+      const newTodo = {
+        id: uuid.v4(),
+        title:title,
+        completed: false
+      }
+      this.setState({todos : [...this.state.todos, newTodo]});
+  }
+
+  render() {
+    return (
+      <Router>
+      <div className="App">
+        <div className="container">
+        <Header/>
+        <Route exact path="/" render = {props => (
+            <Route.Fragment>
+                 <AddTodo addTodo= {this.addTodo} />
+       <Todos todos={ this.state.todos }
+       toggleComplete={this.toggleComplete}
+       deleteTodo={this.deleteTodo}/>
+            </Route.Fragment>
+        )} />
+
+        <Route path="/about" component= {About} />
+        </div>
+      </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
